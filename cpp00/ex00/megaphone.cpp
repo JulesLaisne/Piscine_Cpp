e2524bef78cca73dd33ba7b42e9aa03d02f29e2b
Jules Laisne <jlaisne@z1r4p5.42lyon.fr>
2023-05-21 14:27:17 +0200

    ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
+#include <locale>
+
 
 int main(int argc, char **argv) {
 
 	int		i;
+	std::locale loc;
+	std::string::size_type index; 
 
 	i = 1;
 	if (argc == 1)
@@ -11,9 +16,13 @@ int main(int argc, char **argv) {
 	{
 		while (argv[i])
 		{
-			std::toupper(argv[i]);
-			std::cout << argv[i];
-			std::cout << " ";
+			std::string str = argv[i];
+			index = 0;
+			while (index < str.length())
+			{
+				std::cout << std::toupper(str[index],loc);
+				index++;
+			}
 			i++;
 		}
 		std::cout << std::endl;
