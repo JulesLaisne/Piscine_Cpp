f5fefaab3977d6445a321a9cd3603160d0867106
Jules Laisne <jlaisne@z2r1p5.42lyon.fr>
2023-09-28 10:05:08 +0200

    cpp05

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlaisne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 16:49:38 by juleslaisne       #+#    #+#             */
-/*   Updated: 2023/09/20 10:53:42 by jlaisne          ###   ########.fr       */
+/*   Updated: 2023/09/28 10:03:22 by jlaisne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,31 @@ int main( void ) {
     try {
     
         Bureaucrat Jules = Bureaucrat("Jules", 1);
+        Jules.PromoteGrade();
+        Jules.PromoteGrade();
+    }
+    
+    catch (std::exception & e) {
+        
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+    
+        Bureaucrat Jules = Bureaucrat("Jules", 1);
         Jules.DemoteGrade();
-        Jules.getGrade();
-        Jules.PromoteGrade();
-        Jules.PromoteGrade();
+        std::cout << Jules.getGrade() << std::endl;
+        std::cout << Jules << std::endl;
+    }
+    
+    catch (std::exception & e) {
+        
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+    
+        Bureaucrat Jules = Bureaucrat("Jules", -1);
         std::cout << Jules << std::endl;
     }
     
--- cpp05/ex01/src/main.cpp
+++ cpp05/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlaisne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 16:49:38 by juleslaisne       #+#    #+#             */
-/*   Updated: 2023/09/26 14:50:48 by jlaisne          ###   ########.fr       */
+/*   Updated: 2023/09/28 10:04:04 by jlaisne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,21 @@ int main( void ) {
     try {
         
         Bureaucrat bureaucrat("Jules",11);
+        Form form("Permis", 10, 1);
+        Form form2 = form;
+
+        bureaucrat.PromoteGrade();
+        bureaucrat.signForm(form2);
+
+        std::cout << form2 << std::endl;
+    } 
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try {
+        
+        Bureaucrat bureaucrat("Jules", 150);
         Form form("Permis", 10, 1);
         Form form2 = form;
 
